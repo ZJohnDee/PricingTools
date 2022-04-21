03abbf3f7e6134e001152025caf54653a6b71406
CrazyGamesMC <johndee3a@gmail.com>
2022-04-21 14:56:52 +0200

    Login disclaimer
    Added a disclaimer on the logging screen for legal reasons;
    
    Also added a _redirects file

--- src/pages/login/PageLogin.tsx
+++ src/pages/login/PageLogin.tsx
@@ -1,47 +1,52 @@
 import {FC, useContext} from "react";
 import React from "react";
 import {Button} from "@mui/material";
-import {Language} from "../../libs/language";
+import {Language, LanguageProvider} from "../../libs/language";
 import "./pageLogin.css";
 import {loginWithGoogle, logout} from "../../libs/firebase";
-import {UserContext} from "../../libs/context";
+import {LanguageContext, UserContext} from "../../libs/context";
+import Disclaimer from "../../components/disclaimer/Disclaimer";
 
 
-const PageLogin:FC = (props: any) =>
-{
+const PageLogin: FC = (props: any) => {
 
 
   let {user} = useContext(UserContext);
 
-  if (user == null)
-  {
-      return (
-          <div id={"page-login"}>
-              <h1>LOGIN</h1>
-              <Button
-                  color={"warning"}
-                  variant={"contained"}
-                  onClick={() => loginWithGoogle()}
-              >
-                  Login with Google
-              </Button>
-          </div>
-      )
-  }
-  else
-  {
-      return (
-          <div id={"page-login"}>
-              <h1>Logout</h1>
-              <Button
-                  color={"warning"}
-                  variant={"contained"}
-                  onClick={() => logout()}
-              >
-                  Logout
-              </Button>
-          </div>
-      )
+  const {language} = useContext(LanguageContext);
+  const langProvider = new LanguageProvider(language);
+
+  if (user == null) {
+    return (
+      <div id={"page-login"}>
+        <h1>LOGIN</h1>
+        <Button
+          color={"warning"}
+          variant={"contained"}
+          onClick={() => loginWithGoogle()}
+        >
+          Login with Google
+        </Button>
+        <Disclaimer
+          heading={langProvider.getText("Disclaimers.AccountPrivacy.Heading")}
+          message={langProvider.getText("Disclaimers.AccountPrivacy.Message")}
+          type={"warning"}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <div id={"page-login"}>
+        <h1>Logout</h1>
+        <Button
+          color={"warning"}
+          variant={"contained"}
+          onClick={() => logout()}
+        >
+          Logout
+        </Button>
+      </div>
+    )
   }
 
 }
